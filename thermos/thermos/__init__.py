971c366fa70acc8da2b59f4b1b0b474e8b56a9f1
VLyras <vas.lyras@gmail.com>
2019-08-01 19:10:33 +0100

    First commit

---
+++ thermos/thermos/__init__.py
@@ -0,0 +1,30 @@
+import os
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_moment import Moment
+from flask_debugtoolbar import DebugToolbarExtension
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '\xd3\x869\x06\xb9\xe7~\xde\xc55l\x19l\xc2\xdc\r\x01i:\x9dvA\x98\xbc'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'thermos.db')
+app.config['DEBUG'] = True
+db = SQLAlchemy(app)
+
+# Configure authentication
+login_manager = LoginManager()
+login_manager.session_protection = "strong"
+login_manager.login_view = "login"
+login_manager.init_app(app)
+
+# Enable debugtoolbar
+toolbar = DebugToolbarExtension(app)
+
+# for displaying timestamps
+moment = Moment(app)
+
+import models
+import views
